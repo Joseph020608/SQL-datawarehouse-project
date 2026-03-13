47d3a664971900ba77125fce0296f37dfaacf758
Joseph Reina <santiagoreina002@gmail.com>
2026-03-13 15:58:21 -0500

    Create 02_dimension_exploration.sql

---
+++ scripts/02_dimension_exploration.sql
@@ -0,0 +1,8 @@
+-- Explore countries
+
+SELECT DISTINCT country FROM gold.dim_customers;
+
+-- Explore products
+
+SELECT DISTINCT category, subcategory, product_name FROM gold.dim_products
+ORDER BY 1,2,3;
